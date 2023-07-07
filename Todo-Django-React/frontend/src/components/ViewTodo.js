5ab794acbc9f5c0771a382a0521be6178b317d32
msinankk <sinanjfs@gmail.com>
2023-07-07 13:56:50 +0530

    [updt] style updated

--- Todo-Django-React/frontend/src/components/ViewTodo.js
+++ Todo-Django-React/frontend/src/components/ViewTodo.js
@@ -1,26 +1,25 @@
-
 function ViewTodo(props) {
   const headers = new Headers({
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   });
 
-  function updateTodo(todo){
+  function updateTodo(todo) {
     fetch(`http://localhost:8000/todos/${todo.id}/`, {
-    method: 'PUT',
-    headers: headers,
-    body: JSON.stringify(todo)
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(todo),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   }
 
-  function completedStatus(e,id){
-    const todo = props.todos.map(obj => {
+  function completedStatus(e, id) {
+    const todo = props.todos.map((obj) => {
       // this method will update completed status
       if (obj.id === id) {
-        const todo = {...obj, completed: !obj.completed}
-        updateTodo(todo)
+        const todo = { ...obj, completed: !obj.completed };
+        updateTodo(todo);
         return todo;
       }
       return obj;
@@ -28,40 +27,39 @@ function ViewTodo(props) {
     props.setTodo(todo);
   }
 
-  function deleteTodo(e,id){
+  function deleteTodo(e, id) {
     // this method is used to delete todo
-    const todo = props.todos.filter(obj=> obj.id !== id)
+    const todo = props.todos.filter((obj) => obj.id !== id);
     fetch(`http://localhost:8000/todos/${id}/`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: headers,
-      body: JSON.stringify(todo)
+      body: JSON.stringify(todo),
     })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-    props.setTodo(todo)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+    props.setTodo(todo);
   }
-  function updateTitle(e,id){
+  function updateTitle(e, id) {
     const newValue = e.target.innerText;
-    props.setTodo(todos =>
-      todos.map(obj => {
-        if (obj.id === id){
-          const todo = {...obj, title: newValue}
-          updateTodo(todo)
+    props.setTodo((todos) =>
+      todos.map((obj) => {
+        if (obj.id === id) {
+          const todo = { ...obj, title: newValue };
+          updateTodo(todo);
           return todo;
-
         }
         return obj;
       })
     );
   }
-  function updateDesc(e,id){
+  function updateDesc(e, id) {
     const newValue = e.target.innerText;
-    props.setTodo(todos =>
-      todos.map(obj => {
-        if (obj.id === id){
-          const todo = {...obj, description: newValue}
-          updateTodo(todo)
+    props.setTodo((todos) =>
+      todos.map((obj) => {
+        if (obj.id === id) {
+          const todo = { ...obj, description: newValue };
+          updateTodo(todo);
           return todo;
         }
         return obj;
@@ -75,27 +73,58 @@ function ViewTodo(props) {
         {props.todos.map((todo) => (
           <li
             key={todo.id}
-            className="list-group-item d-flex justify-content-between"
+            className="list-group-item d-flex justify-content-between mb-2"
+            style={{
+              "border-radius":"5px",
+              "border-left-width": "10px",
+              "border-left-style": "solid",
+              "border-left-color": todo.completed ? "#9FD6AE":'',
+            }}
           >
             <div>
-              <h5 
-              className="edit-content"
-               contentEditable="true"
-               onBlur={(e) => updateTitle(e,todo.id)}
-               >{todo.title}</h5>
+              <dev id="statusContainer">
+                <input
+                  type="checkbox"
+                  className="tgl tgl-ios"
+                  id={todo.id}
+                  checked={todo.completed}
+                  onChange={(e) => completedStatus(e, todo.id)}
+                />
+                <label className="tgl-btn" for={todo.id}></label>
+                <span className="slider round" />
+              </dev>
+              <h5
+                className="edit-content"
+                contentEditable="true"
+                onBlur={(e) => updateTitle(e, todo.id)}
+              >
+                {todo.title}
+              </h5>
               <p
-              className="edit-content"
-              contentEditable="true"
-              onBlur={(e) => updateDesc(e,todo.id)}>{todo.description}</p>
+                className="edit-content"
+                contentEditable="true"
+                onBlur={(e) => updateDesc(e, todo.id)}
+              >
+                {todo.description}
+              </p>
+              <button
+                className="btn btn-outline-dark"
+                // onClick={(e) => deleteTodo(e, todo.id)}
+              >
+                Archive
+              </button>
+              <button
+                className="btn btn-danger ml-2"
+                onClick={(e) => deleteTodo(e, todo.id)}
+              >
+                Remove
+              </button>
             </div>
             <div>
-              <label className="switch">
-                <input type="checkbox"
-                 checked={todo.completed}
-                 onChange={(e)=>completedStatus(e,todo.id)} />
-                <span className="slider round" />
-              </label>
-              <button className="btn btn-danger ml-3" onClick={(e)=>deleteTodo(e,todo.id)}>Delete</button>
+              {/* <label className="switch"> */}
+                
+              {/* </label> */}
+              
             </div>
           </li>
         ))}
