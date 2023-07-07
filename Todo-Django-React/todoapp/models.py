495b8c310558e657e2fe5d06d17047a2ebbc053f
msinankk <sinanjfs@gmail.com>
2023-07-07 11:49:57 +0530

    [updt] todo defualt ordering

--- Todo-Django-React/todoapp/models.py
+++ Todo-Django-React/todoapp/models.py
@@ -1,13 +1,28 @@
+"""
+models.py
+
+This module is used to register django models
+"""
 from django.db import models
 
 # Create your models here.
 
 
 class Todo(models.Model):
+    """
+    Todo model
+    """
+
     title = models.CharField(max_length=20)
     description = models.TextField()
     completed = models.BooleanField()
 
     def __str__(self) -> str:
         return self.todo
-        
+
+    class Meta:
+        """
+        Meta class to add additional options
+        """
+
+        ordering = ["-id"]
